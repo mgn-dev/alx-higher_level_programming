5bcfa00e7a4ce3d380c5635f9a5c0703e8e33194
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-07 02:42:59 +0000

    fix task 8 of project 0x01

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,9 +4,6 @@ islower = __import__('7-islower').islower
 
 def uppercase(str):
     for i in range(len(str)):
-        if islower(str[i]):
-            s = ord(str[i]) - 32
-            print("{:c}".format(s), end='')
-        else:
-            print(str[i], end='')
+        s = ord(str[i])
+        print("{:c}".format(s - 32 if islower(str[i]) else s), end='')
     print('')
