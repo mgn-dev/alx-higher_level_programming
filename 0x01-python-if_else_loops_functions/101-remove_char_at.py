c9aa125eb9bf3bc9cdb65f5464dd3ee839c29fc7
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-08 02:19:22 +0000

    fix task 15 of project 0x01

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new_str = []
+    new_str = ""
     j = 0
-    for i in range(len(str)):
-        if i != n:
-            new_str.append(str[i])
+    for c in str:
+        if j != n:
+            new_str += c
+        j += 1
     return new_str
