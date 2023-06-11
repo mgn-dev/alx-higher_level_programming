0f6bcdb38959d138992d7a9aacf464a4a9a794dd
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-11 14:01:28 +0000

    fix task 3 of project 0x03

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print_reversed = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+printr = __import__('3-print_reversed_list_integer'
+                    ).print_reversed_list_integer
 
 my_list = [1, 2, 3, 4, 5]
-print_reversed(my_list)
+printr(my_list)
