e30dbfe2a325c393a46f95e649bc0a009d54b444
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-11 13:57:38 +0000

    fix task 3 of project 0x03

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for item in range(len(my_list), 0, -1):
-        print("{:d}".format(item))
+    if len(my_list) > 0:
+        for item in reversed(my_list):
+            print("{:d}".format(item))
