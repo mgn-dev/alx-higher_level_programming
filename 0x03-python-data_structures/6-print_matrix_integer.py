3b73e28fb8f0ac8a674abd1a825e9e9f8aea730a
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-11 14:32:37 +0000

    fix task 6 of project 0x03

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end='')
+        inner_len = len(matrix[i])
+        for j in range(inner_len):
+            print("{:d}{}".format(matrix[i][j], "" if j == inner_len - 1
+                                  else " "), end='')
         else:
             print("")
