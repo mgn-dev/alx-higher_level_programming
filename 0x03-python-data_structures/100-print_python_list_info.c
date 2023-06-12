bba82c512f6930f9bfc225fcf5701b01b9eb9682
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-12 19:13:25 +0000

    do task 14 of project 0x03

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints information about a PyObject struct
+ * @p: pointer to a PyObject struct.
+ *
+*/
+void print_python_list_info(PyObject *p)
+{
+    PyListObject *p_obj = (PyListObject *)p;
+    //int i, allocated, num_items;
+
+    // allocated = p_obj->allocated;
+    // num_items = p_obj->ob_base.ob_size;
+
+    // printf("[*] Size of the Python List = %d", num_items);
+    // printf("[*] Allocated = %d", allocated);
+
+    // for (i = 0; i < num_items; i++)
+    // {
+    //     printf("Element %s: ", p_obj->ob_base.ob_base.ob_type->tp_name);
+    // }
+}
