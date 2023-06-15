cfb141f85f4656618d84952423ebbb436505dd3f
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-15 02:47:31 +0000

    do task 12 of project 0x04

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+
+    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    keys = list(rom_int.keys())
+    sum = 0
+
+    str_len = len(roman_string)
+    key_len = len(keys)
+
+    if str_len == 2:
+        if rom_int[roman_string[0]] < rom_int[roman_string[1]]:
+            sum += rom_int[roman_string[1]] - rom_int[roman_string[0]]
+        else:
+            sum += rom_int[roman_string[1]] + rom_int[roman_string[0]]
+    else:
+        for i in range(str_len):
+            for j in range(key_len):
+                if roman_string[i] == keys[j]:
+                    sum += rom_int[keys[j]]
+
+    return sum
