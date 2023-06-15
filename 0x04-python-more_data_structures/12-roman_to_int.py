d9417667028f7f339c332c6ba9fbfd67312cf3d1
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-15 02:56:37 +0000

    do task 12 of project 0x04

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
 
-    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     keys = list(rom_int.keys())
     sum = 0
 
