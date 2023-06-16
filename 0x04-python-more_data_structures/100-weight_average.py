9952b69f502d92e88fe32bc27ab92820129e2b48
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-16 15:23:26 +0000

    fix task 13 of project 0x04

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,8 +2,10 @@
 def weight_average(my_list=[]):
     sum_of_mul = 0
     sum_of_b = 0
+    result = 0
     if my_list:
         for a, b in my_list:
             sum_of_mul += (a * b)
             sum_of_b += b
-    return (sum_of_mul / sum_of_b)
+        result = sum_of_mul / sum_of_b
+    return (result)
