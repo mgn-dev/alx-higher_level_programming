6b688ebcfab848938840a3bae46560b7274b5b25
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-28 14:11:12 +0000

    do task 6 of project 0x06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,7 +72,7 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            if (self.__position[1] > 0):
+            for g in range(self.__position[1]):
                 print("")
             for i in range(self.__size):
                 for h in range(self.__position[0]):
