fa9c1627baf38851fd18f53e293e9bc1f92d8d35
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-06-28 01:00:55 +0000

    do task 1 of project 0x06

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Square - an empty class Square that defines a square.
+"""
+
+
+class Square:
+    """
+    Square - an empty class Square that defines a square.
+    """
+    def __init__(self, size):
+        """
+        Square - an empty class Square that defines a square.
+
+        Args:
+            size (int): the size of the square.
+        """
+        self.__size = size
+        """int: stores the size of the attribute"""
