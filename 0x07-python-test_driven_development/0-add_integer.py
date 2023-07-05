3347f508680f78712589187abe2da1f02f7ddf15
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-05 16:14:38 +0000

    Fix task 0, project 0x09

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""This module contains the addition function"""
+
 def add_integer(a, b=98):
+    """Adds two integers
+
+    Params:
+        a (int): first number
+        b (int): second number"""
 
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-        
+
     return (int(a) + int(b))
