625ee2fb1b9ce6ad532a42141a13e73bb42095cd
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-10 15:58:15 +0000

    Do task 0, project 0x0A

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module contains lookup function
+    that looks up and list
+    all attributes of an object"""
+
+
+def lookup(obj):
+    """ This function wraps a dir object."""
+    return dir(obj)
