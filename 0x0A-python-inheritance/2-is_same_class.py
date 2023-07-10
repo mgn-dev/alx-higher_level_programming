42838e322331752e262290239d3b86c15fa23ae3
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-10 21:03:05 +0000

    Fix task 2, project 0x0A

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,4 +8,4 @@ def is_same_class(obj, a_class):
     Params:
         obj (any object): an object instance.
         a_class (any class): a class"""
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
