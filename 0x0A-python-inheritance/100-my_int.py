0bc6801ba2bc1c5901deddb481d975a9f431e683
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-11 01:56:01 +0000

    Fix task 12, project 0x0A

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,10 +2,15 @@
 """ Module contains MyInt Class.
     that wraps the int object."""
 
+
 class MyInt(int):
+    """ Class that inherets from int and
+        overrides some methods."""
 
     def __ne__(self, other):
+        """ negates operator"""
         return super().__eq__(other)
 
     def __eq__(self, other):
+        """ negates operator"""
         return super().__ne__(other)
