97f15eb847ece8a7875d476b39f4a97c346d5cde
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-12 01:05:10 +0000

    Do task 4, project 0x0B

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module contains a function that
+    converts a JSON representation of an object
+    to python object."""
+
+import json
+
+
+def from_json_string(my_str):
+    """ Converts a JSON object to python
+        object.
+
+        Params:
+            my_obj (any): object
+
+        Returns:
+            json object."""
+    result = json.loads(my_str)
+    return result
