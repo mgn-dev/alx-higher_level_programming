04b61f878edc7a376f6aba1be479168ad05e8b04
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-21 23:33:20 +0000

    Do task 11, project 0x0C

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,18 @@ class Square(Rectangle):
         """ Prints Square"""
         return (f"[Square] ({self.id}) {self.x}/{self.y}"
                 f" - {self.width}")
+
+    @property
+    def size(self):
+        """ Getter for width."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter for width and height."""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
