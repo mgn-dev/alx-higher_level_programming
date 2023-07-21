8619cf3a919fe7970ddd99265def6f2a78175bd1
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-21 19:43:06 +0000

    Do task 8, project 0x0C

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,8 +106,13 @@ class Rectangle(Base):
         """ Updates this Rectangle's attributes
 
             args (tuple): accepts multiple values."""
-        self.id = args[0]
-        self.width = args[1]
-        self.height = args[2]
-        self.x = args[3]
-        self.y = args[4]
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
