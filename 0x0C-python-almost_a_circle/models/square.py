78b447789875d7b8c970b1d5b78f2ec0f8d257f9
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-21 23:46:20 +0000

    Do task 12, project 0x0C

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,3 +33,27 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Updates this Square's attributes
+
+            args (tuple): accepts multiple values.
+            kwargs (dictionary): acceps multiple keyworded values."""
+        if args is not None and len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
