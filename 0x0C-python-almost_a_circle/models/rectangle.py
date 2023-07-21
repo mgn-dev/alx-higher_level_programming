a54b9d8f5cc7a158d4d96d382c26def62d01ce7c
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-07-21 22:37:14 +0000

    Do task 9, project 0x0C

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,7 +107,7 @@ class Rectangle(Base):
 
             args (tuple): accepts multiple values.
             kwargs (dictionary): acceps multiple keyworded values."""
-        if args != None and len(args) > 0:
+        if args is not None and len(args) > 0:
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
