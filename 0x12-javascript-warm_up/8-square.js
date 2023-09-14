d049bb1084e56f371d43363d943ead00ddbbf23e
Mojalefa <126557401+mgn-dev@users.noreply.github.com>
2023-09-14 18:49:12 +0000

    Fix task 8, 0x12

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const { argv } = require('process');
 
 const len = +argv[2];
 
-if (argv.length === 2 && isNaN(len)) {
+if (argv.length === 2 || isNaN(len)) {
   console.log('Missing size');
 } else if (argv.length === 3 && !isNaN(len)) {
   let x = '';
