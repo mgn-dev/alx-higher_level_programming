cfdff079881c926774b1ce8627a46fdf2faabdca
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-09-18 20:50:26 +0200

    Do task 5, 0x0F ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     mysql_username = argv[1]
     mysql_password = argv[2]
     database_name = argv[3]
-    arg =  argv[4]
+    arg = argv[4]
 
     db = MySQLdb.connect(host='localhost',
                          user=mysql_username,
