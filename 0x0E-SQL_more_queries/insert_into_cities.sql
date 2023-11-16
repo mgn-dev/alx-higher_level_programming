d275afdf4b8ecbacc64e48812dd94cbec724e263
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-11-16 02:15:49 +0200

    Do tasks 0-13, project 0x0E

---
+++ 0x0E-SQL_more_queries/insert_into_cities.sql
@@ -0,0 +1,9 @@
+-- insert into cities
+INSERT INTO cities (state_id, name)
+VALUES
+(1, "San Francisco"),
+(1, "San Jose"),
+(2, "Page"),
+(3, "Paris"),
+(3, "Houston"),
+(3, "Dallas");
---
+++ 0x0E-SQL_more_queries/insert_into_states.sql
@@ -0,0 +1,12 @@
+-- insert into states table
+INSERT INTO states (
+    name
+) VALUES (
+    "California"
+),
+(
+    "Arizona"
+),
+(
+    "Texas"
+);
