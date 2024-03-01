e6efca44d31e7964b8174ad0d2e888e81c0cacb5
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-03-01 22:55:11 +0200

    Do task 1, project 0x11, python urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Python script that fetches a resource
 """
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    data = response.read()
-    content_type = type(data)
-    utf8_content = data.decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        content_type = type(data)
+        utf8_content = data.decode('utf-8')
 
-print("Body response:")
-print(f"\t- type: {content_type}")
-print(f"\t- content: {data}")
-print(f"\t- utf8 content: {utf8_content}")
+    print("Body response:")
+    print(f"\t- type: {content_type}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {utf8_content}")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches a resource
 """
-import urllib.request
-import sys
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    request_id = response.headers.get('X-Request-Id')
+    with urllib.request.urlopen(url) as response:
+        request_id = response.headers.get('X-Request-Id')
 
-print(f"{request_id}")
+    print(f"{request_id}")
