5ca017fd3ca2ddf836caf491fcbb57e6ff366ca4
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-03-02 01:44:03 +0200

    Do task 7, project 0x11, python requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 
     response = requests.get(url)
     if response.status_code >= 400:
-        print("Error code:", response.status_code)
+        print("Error code: ", response.status_code)
     else:
         print(response.text)
