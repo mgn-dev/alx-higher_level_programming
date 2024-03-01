f3619b8cd4c39b8103ffd6f9f637c2af5181a32d
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-03-01 22:51:37 +0200

    Do task 1, project 0x11, python urllib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,5 @@ url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
     request_id = response.headers.get('X-Request-Id')
-    if request_id:
-        print(f"{request_id}")
+
+print(f"{request_id}")
