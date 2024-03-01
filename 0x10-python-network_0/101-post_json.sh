c2aa116b6cb788abb1959598d713c82ee2a86493
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-03-01 21:37:09 +0200

    Do task 8, project 0x10, curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that displays the size of the body of the response
-curl -s -d "$2" -H "Content-Type: application/json" -X POST "$1"
+curl -s -d "$(cat "$2")" -H "Content-Type: application/json" -X POST "$1"
