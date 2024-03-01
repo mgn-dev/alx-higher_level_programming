07b8e8058deedd613059114c52c995d66e6238ce
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-03-01 23:55:01 +0200

    Do task 4-6, project 0x11, python requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches a resource
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
