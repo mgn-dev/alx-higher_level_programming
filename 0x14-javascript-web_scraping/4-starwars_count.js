fdaabaaed3f723240f696f05f81ce59df7aa9184
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-05-21 22:18:52 +0200

    Do task 4, project 0x14, Web Scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
+});
