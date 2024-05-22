afa0e77f07f00232699ab2041f714ab9360af627
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-05-22 21:36:55 +0200

    Do task 0, project 0x15, API

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,3 @@
 #!/usr/bin/node
 
-const request = require('request');
 
-function getCharacterName (url) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log(JSON.parse(body).name);
-    }
-  });
-}
-
-function getCharacters (movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const characters = JSON.parse(body).characters;
-      for (let i = 0; i < characters.length; i++) {
-        getCharacterName(characters[i]);
-      }
-    }
-  });
-}
-
-const movieId = process.argv[2];
-getCharacters(movieId);
