9db19949f3e8b8a33627f13485e0fe2744b0a9c0
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-06-24 01:46:54 +0200

    Do task 16, project 0x0F, Python ORM

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module that adds State object “Louisiana” to the database
+"""
+
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dn_name = sys.argv[3]
+
+    # Create a MySQL database engine using command-line arguments
+    # for username, password, and database name respectively
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, dn_name),
+                           pool_pre_ping=True)
+    # Create all tables defined in the Base model in the connected database
+    Base.metadata.create_all(engine)
+    # Create a sessionmaker to interact with the database
+    Session = sessionmaker(bind=engine)
+    # Create a new session
+    session = Session()
+
+    res = session.query(State).outerjoin(City).\
+        order_by(State.id, City.id).all()
+
+    for state in res:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+
+    # Close the session
+    session.close()
