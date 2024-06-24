f599eb6b2d3e917ad7cabe6973efcc2188a790d8
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-06-24 02:12:38 +0200

    Do task 16, project 0x0F, Python ORM

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python3
-"""
-Lists all State objects and corresponding City objects contained in the DB
-"""
-import sys
-from relationship_state import Base, State
-from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""Module that lists all State objects, and corresponding City objects
 
+<state id>: <state name>
+<tabulation><city id>: <city name>
+"""
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dn_name = sys.argv[3]
+
+    # Create a MySQL database engine using command-line arguments
+    # for username, password, and database name respectively
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, dn_name),
                            pool_pre_ping=True)
+    # Create all tables defined in the Base model in the connected database
     Base.metadata.create_all(engine)
-
+    # Create a sessionmaker to interact with the database
     Session = sessionmaker(bind=engine)
+    # Create a new session
     session = Session()
 
-    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    states = session.query(State).outerjoin(City).\
+        order_by(State.id, City.id).all()
 
-    for state in st:
-        print("{}: {}".format(state.id, state.name))
+    for state in states:
+        print(f"{state.id}: {state.name}")
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print(f"\t{city.id}: {city.name}")
+
+    # Close the session
+    session.close()
